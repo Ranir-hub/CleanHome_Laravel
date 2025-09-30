3f21ee29aeacd80f2da93a84b4a5558cb954c01e
Ranir-hub <arina2005vvv@gmail.com>
2025-09-30 19:29:45 +0500

    Migrations task

---
+++ database/migrations/2025_09_30_133800_create_tables.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('phone');
+            $table->boolean('is_organization');
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 256);
+        });
+
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->string('name', 256);
+            $table->unsignedBigInteger('price');
+            $table->integer('balance');
+        });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->dateTime('time_start');
+            $table->dateTime('time_end');
+            $table->integer('total_price');
+        });
+
+        Schema::create('item_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->unsignedInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function(Blueprint $table){
+            $table->dropColumn('phone');
+            $table->dropColimn('is_organization');
+        });
+        Schema::dropIfExists('orders');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('item_orders');
+    }
+};
